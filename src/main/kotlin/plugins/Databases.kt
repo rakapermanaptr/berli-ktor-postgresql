c135d2b14b59e446f0c71fb804732672f3732fc0
Raka Permana <raka@digivest.id>
2025-06-09 13:24:21 +0700

    Fix: env

--- src/main/kotlin/plugins/Databases.kt
+++ src/main/kotlin/plugins/Databases.kt
@@ -4,11 +4,15 @@ import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
 import io.ktor.server.application.Application
 import org.jetbrains.exposed.sql.Database
+import java.net.URI
 
 fun Application.configureDatabases() {
-    val dbUrl = System.getenv("DB_URL") ?: "jdbc:postgresql://localhost:5432/berli_database"
-    val dbUser = System.getenv("DB_USER") ?: "admin"
-    val dbPassword = System.getenv("DB_PASSWORD") ?: "secret"
+    val rawUrl = System.getenv("DATABASE_URL") ?: error("Missing DATABASE_URL")
+    val uri = URI(rawUrl)
+
+    val dbUrl = "jdbc:postgresql://${uri.host}:${uri.port}${uri.path}"
+    val dbUser = uri.userInfo.split(":")[0]
+    val dbPassword = uri.userInfo.split(":")[1]
 
     val config = HikariConfig().apply {
         jdbcUrl = dbUrl
