adc2f4f4290009a35e5f26603fe7dd4469d873e3
Raka Permana <raka@digivest.id>
2025-06-10 11:18:01 +0700

    feat: changed model for report

--- src/main/kotlin/models/domain/Report.kt
+++ src/main/kotlin/models/domain/Report.kt
@@ -5,8 +5,14 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class Report(
     val id: Int? = null,
-    val report: String,
-    val reportDetails: String,
+    val username: String,
+    val userId: Int,
+    val complaint: String,
+    val complaintDetails: String,
+    val image: String,
     val location: String,
-    val reporter: String,
+    val locationDetails: String,
+    val status: String,
+    val likeNumber: Int = 0,
+    val dateCreated: String
 )
--- src/main/kotlin/models/entity/ReportEntity.kt
+++ src/main/kotlin/models/entity/ReportEntity.kt
@@ -8,24 +8,42 @@ import org.jetbrains.exposed.dao.id.IntIdTable
 
 fun reportDaoToModel(dao: ReportDAO) = Report(
     id = dao.id.value,
-    report = dao.report,
-    reportDetails = dao.reportDetail,
-    reporter = dao.reporter,
-    location = dao.location
+    username = dao.username,
+    userId = dao.userId,
+    complaint = dao.complaint,
+    complaintDetails = dao.complaintDetail,
+    image = dao.image,
+    location = dao.location,
+    locationDetails = dao.locationDetail,
+    status = dao.status,
+    likeNumber = dao.likeNumber,
+    dateCreated = dao.dateCreated
 )
 
 object ReportTable : IntIdTable("report") {
-    val report = varchar("report", 100)
-    val reportDetail = varchar("reportDetails", 300)
-    val reporter = varchar("reporter", 100)
-    val location = varchar("location", 100)
+    val username = varchar("username", 50)
+    val userId = integer("userId")
+    val complaint = varchar("complaint", 100)
+    val complaintDetail = varchar("complaintDetail", 300)
+    val image = varchar("image", 500)
+    val location = varchar("location", 300)
+    val locationDetail = varchar("locationDetail", 100)
+    val status = varchar("status", 20)
+    val likeNumber = integer("likeNumber")
+    val dateCreated = varchar("dateCreated", 100)
 }
 
 class ReportDAO(id: EntityID<Int>) : IntEntity(id) {
     companion object: IntEntityClass<ReportDAO>(ReportTable)
 
-    var report by ReportTable.report
-    var reportDetail by ReportTable.reportDetail
-    var reporter by ReportTable.reporter
+    var username by ReportTable.username
+    var userId by ReportTable.userId
+    var complaint by ReportTable.complaint
+    var complaintDetail by ReportTable.complaintDetail
+    var image by ReportTable.image
     var location by ReportTable.location
+    var locationDetail by ReportTable.locationDetail
+    var status by ReportTable.status
+    var likeNumber by ReportTable.likeNumber
+    var dateCreated by ReportTable.dateCreated
 }
